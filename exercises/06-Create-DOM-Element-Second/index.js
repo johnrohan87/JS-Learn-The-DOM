811c5d4e50ae500805e873deb7909a215bb4ae3d
John Rohan <john.rohan87@gmail.com>
2020-06-06 05:14:54 +0000

    updating

--- exercises/06-Create-DOM-Element-Second/index.js
+++ exercises/06-Create-DOM-Element-Second/index.js
@@ -1,3 +1,4 @@
 //you code here
 document.body.innerHTML = "<h1>Hello World</h1>";
-
+var list = document.getElementsByTagName("H1")[0];
+list.innerHTML = '<img src="http://placehold.it/350x150" alt="place holder">';
