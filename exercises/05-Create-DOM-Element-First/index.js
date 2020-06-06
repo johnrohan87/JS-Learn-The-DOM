3dfdb8cfcefecb3320c6ddcdc129625b1b975e7b
John Rohan <john.rohan87@gmail.com>
2020-06-06 04:57:14 +0000

    updating

--- exercises/05-Create-DOM-Element-First/index.js
+++ exercises/05-Create-DOM-Element-First/index.js
@@ -4,3 +4,8 @@ document.querySelector("#myDiv").appendChild(elem);
 
 //Your code here
 // Don'change the code above
+
+var p1 = document.createElement("P");
+p1.innerHTML = "Hello World";
+p1.style.backgroundColor = "yellow";
+document.querySelector("#myDiv").appendChild(p1);
